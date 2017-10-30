acc9adf475db4da730a38b4914fa6b7a9ee8a3e7
Posquit0.BJ <posquit0.bj@gmail.com>
2017-10-31 01:02:11 +0900

    Bootstrap

---
+++ app.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const dotenv = require('dotenv');
+const slackin = require('slackin')
+
+
+// Load environment variables from .env file
+dotenv.config();
+
+const port = process.env.SLACKIN_PORT || 5555;
+const token = process.env.SLACKIN_TOKEN || '';
+
+const app = slackin.default({
+    org: 'hashicorpkr',
+    token: token,
+    path: '/',
+    interval: 1000,
+    channels: '',
+    silent: false
+});
+
+// Start server
+if (!module.parent) {
+  app.listen(port, () => {
+    console.log(`App server listening on ${port}`);
+  });
+}
+
+// Expose app
+module.exports = app;
